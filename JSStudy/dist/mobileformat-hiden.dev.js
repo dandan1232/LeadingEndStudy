8ebd6c1343e7ed17bec00b038aea4a54e54af810
dandan1232 <345652127@qq.com>
2021-03-22 20:12:47 +0800

    js常用函数冲冲冲210322

---
+++ JSStudy/dist/mobileformat-hiden.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/**
+ * JS将⼿机号格式化，中间部分以 * 号代替
+ * @param phone
+ * @returns {string | * | void}
+ */
+function phoneToStar(phone) {
+  return phone.replace(/(\d{3})\d{4}(\d{4})/, "$1****$2");
+}
+
+console.log(phoneToStar('13951905171')); // 139****5171
